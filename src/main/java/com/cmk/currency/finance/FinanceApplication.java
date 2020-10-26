99b90fed0cb108c5f609e2ea7e0f695e6d7eef7a
Karthikeyani <cmkarthidoc@gmail.com>
2020-10-26 15:16:36 -0500

    Reading file added in Service class

--- src/main/java/com/cmk/currency/finance/FinanceApplication.java
+++ src/main/java/com/cmk/currency/finance/FinanceApplication.java
@@ -1,13 +1,18 @@
 package com.cmk.currency.finance;
 
+import com.cmk.currency.finance.service.ReadBuildService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class FinanceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(FinanceApplication.class, args);
+		ReadBuildService readBuildService = new ReadBuildService();
+		readBuildService.readBuildID();
 	}
 
 }
--- src/main/java/com/cmk/currency/finance/service/ReadBuildService.java
+++ src/main/java/com/cmk/currency/finance/service/ReadBuildService.java
@@ -2,15 +2,25 @@ package com.cmk.currency.finance.service;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.util.FileCopyUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class ReadBuildService {
 
-    void readBuildID() throws IOException
+    public void readBuildID() throws IOException
     {
-    Resource resource = new ClassPathResource("classpath:\\temp\\exchange\\buildID.txt");
-    InputStream inputStream = resource.getInputStream();
+        Resource resource = new ClassPathResource("classpath:\\temp\\exchange\\buildID.txt");
+        InputStream inputStream = resource.getInputStream();
+
+        try {
+            byte[] bdata = FileCopyUtils.copyToByteArray(inputStream);
+            String data = new String(bdata, StandardCharsets.UTF_8);
+            System.out.println("Data:   "+data);
+        } catch (IOException e) {
+            System.out.println("IOException"+ e);
+        }
     }
 }
